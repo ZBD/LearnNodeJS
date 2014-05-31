f2f1da94c7c2887ae09b1ed05df81b42dd779543
Bindi <bindi.zh@gmail.com>
2014-05-30 21:45:46 -0700

    modify README.mk

--- todo_App/README.mk
+++ todo_App/README.mk
@@ -1,3 +1,5 @@
 This is a todo list application implemented by Bindi using MongoDB, Express, Angular, Node.js.
+Hosted on openshift,
+Try to add user Auth now
 
 Speciall thanks to the tutorial on scotch.io
